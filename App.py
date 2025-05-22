d3cfc55bf07905534f64c34343bfe918ed58c6e6
nadivaazahra <nadivaazahra@students.unnes.ac.id>
2025-05-22 09:15:02 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("Hello, World!")
-st.set_page_config(page_title=("Iris Dashboard App"))
+st.set_page_config(page_title="Iris Dashboard App")
 st.sidebar.header("Dashboard")
